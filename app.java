95870b4bbd2d719a0d9437d1e8105d5b4cbe51a4
Rukiyat Adams <alliruki06@gmail.com>
2023-03-07 11:35:20 -0800

    Committing db.sql

--- app.java
+++ app.java
@@ -1,4 +1,5 @@
 # This is a code for web application
 # Please do not hard code for any reasons
-# Make use of variablse and dynamic variables
+# Make use of variables and dynamic variables
 # Author: Rukiyat Adams
+# Date March 7th 2023.
